c8e43e816e3b67257263a887b5b2f4caa939ce55
Digastrick <110335028+Digastrick@users.noreply.github.com>
2023-12-23 17:16:50 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  server: {
+    proxy: {
+      '/users': {
+        target: 'https://jsonplaceholder.typicode.com/users',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/users/, ''),
+      },
+      '/users/:id':{
+        target:'https://jsonplaceholder.typicode.com/users/:id',
+        changeOrigin:true,
+        rewrite:(path) => path.replace(/^\/user/, ''),
+      },
+      '/posts':{
+        target:`https://jsonplaceholder.typicode.com/posts`,
+        changeOrigin:true,
+        rewrite:(path) => path.replace(/^\/posts/, ''),
+      },
+      '/albums':{
+        target:`https://jsonplaceholder.typicode.com/albums`,
+        changeOrigin:true,
+        rewrite:(path) => path.replace(/^\/albums/, ''),
+      },
+      '/photo':{
+        target:'https://jsonplaceholder.typicode.com/photos',
+        changeOrigin:true,
+        rewrite:(path) => path.replace(/^\/photos/, ''),
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
